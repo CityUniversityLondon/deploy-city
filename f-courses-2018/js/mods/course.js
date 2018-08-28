07e3ef5b75550bd4356d6e9335dfbdc4b1bbdd40
CUL Web Team <ucs-webteam@city.ac.uk>
2018-08-28 10:21:39 +0000

    08/28/18 10:21:04 | Merge branch 'feature/person-profiles' into feature/courses-2018

--- f-courses-2018/js/mods/course.js
+++ f-courses-2018/js/mods/course.js
@@ -42,6 +42,7 @@ var onResize = require('./utils/on-resize'),
     },
 
     initTestimonials = function () {
+
         var w = $('.course__profiles').width(),
             n = $('.course__profiles__item').length;
 
@@ -63,6 +64,13 @@ var onResize = require('./utils/on-resize'),
         }
     },
 
+    testimonialsBackgroundColor = function () {
+        // Get title text colour of selected list item, i.e. nested in aria-hidden="false"
+        var selectedItemColor = $('li.course__profiles__item:not([aria-hidden*="true"]) .course__profiles__item__text__title').css("color");
+
+        // Apply selected text colour to testimonials wrapper background
+        $('.course__testimonials__wrapper').css("background", selectedItemColor);
+    },
 
     initAssessment = function () {
         var container = document.getElementById('course-assessment-chart');
@@ -231,3 +239,8 @@ var onResize = require('./utils/on-resize'),
     };
 
 defer(init);
+
+// Change testimonial wrapper background colour when user scrolls through items
+$(".course__testimonials__wrapper").click(function() {
+    testimonialsBackgroundColor();
+})
