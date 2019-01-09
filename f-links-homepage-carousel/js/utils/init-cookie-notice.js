f49d5906d15c64fb39cfcd9b61159d741aa4b630
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-09 11:13:21 +0000

    01/09/19 11:12:53 | JS: recording elements cont...

--- f-links-homepage-carousel/js/utils/init-cookie-notice.js
+++ f-links-homepage-carousel/js/utils/init-cookie-notice.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
     console.log($('.banner-content a').length);
 
     $(document).on('click', function(e){
-        e.preventDefault();  
+        e.preventDefault(); 
+        var x = e.targetTouches[0].target.tagName;
         console.log('You clicked on a: '+ e.target.tagName + ' element ,with class name: '+ e.target.className +' ,or ID name:' +e.target.id );
-        console.log('You touched on a: '+ e.targetTouches[0].target.tagName);
+        console.log('the href atrribute is: '+ e.target.getAttribute('href'));
+        console.log('You touched on a: '+ x);
 
           
     });
