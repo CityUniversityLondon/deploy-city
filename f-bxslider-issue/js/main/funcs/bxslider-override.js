e30090c4f468f8028d419751872c973d8440cb3a
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-14 15:17:33 +0000

    01/14/19 15:16:44 | JS: touch events updates

--- f-bxslider-issue/js/main/funcs/bxslider-override.js
+++ f-bxslider-issue/js/main/funcs/bxslider-override.js
@@ -6,9 +6,9 @@ module.exports = function () {
         $(document).ready(function(){
             console.log($('.banner-content a').length);
                     
-            $('.banner-content a').on('click', 'touchend', function(e){
-                $('.bx-controls').removeClass('disabled');
-                console.log('clicked');
+            $('.banner-content p a').on('touchstart', function(e){
+                // $('.bx-controls').removeClass('disabled');
+                console.log('touch-start');
                 
                 e.preventDefault();
                 console.log($(this).attr('href'));
@@ -17,50 +17,13 @@ module.exports = function () {
             });
 
             
-
-            $('.banner-content p a').on('touchend', function(e){
-                $('.bx-controls').removeClass('disabled');
-
-                console.log('touch-End');
-                
-                e.preventDefault();
-                console.log($(this).attr('href'));
-                var x = $(this).attr('href');
-                location.href = x;
-            });
-
-            $('.banner-content p a').on('touchstart', function(e){
-                $('.bx-controls').removeClass('disabled');
-                console.log('touch-start');
-                
-                e.preventDefault();
-                console.log($(this).attr('href'));
-                var x = $(this).attr('href');
-                location.href = x;
-            });
-            /*
             $('.bx-wrapper .bx-viewport').on('touchend', function(e){
                 $('.bx-controls').removeClass('disabled');
 
                 console.log('bx-viewport - touch-End');
                 
-                e.preventDefault();
-                console.log($(this).find('.banner-content p a').attr('href'));
-                var x = $(this).find('.banner-content p a').attr('href');
-                location.href = x;
             });
 
-            $('.bx-wrapper .bx-viewport').on('touch-start', function(e){
-                $('.bx-controls').removeClass('disabled');
-
-                console.log('bx-viewport - touch-start');
-                
-                e.preventDefault();
-                console.log($(this).find('.banner-content p a').attr('href'));
-                var x = $(this).find('.banner-content p a').attr('href');
-                location.href = x;
-                
-            }); */
         });
 
 
