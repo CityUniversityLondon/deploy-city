40d40271fb0d0b2b2a1888891edd33dcca881599
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-14 16:28:51 +0000

    01/14/19 16:28:14 | JS: touch events cont...

--- f-bxslider-issue/js/main/funcs/bxslider-override.js
+++ f-bxslider-issue/js/main/funcs/bxslider-override.js
@@ -4,19 +4,39 @@ module.exports = function () {
     
     init = function () {
         $(document).ready(function(){
+            var prevTouchX;
+            var prevTouchY;
+            console.log('X cor is: '+ prevTouchX);
+            console.log('Y cor is: '+ prevTouchY);
+            $(document).on('touchstart', function(e){
+                prevTouchX = e.touches[0].clientX;
+                prevTouchY = e.touches[0].clientY;
+                console.log('X cor is: '+ prevTouchX);
+                console.log('Y cor is: '+ prevTouchY);
+
+            });
+
             console.log($('.banner-content a').length);
+            console.log($('.bx-wrapper .bx-viewport').lenght);
                     
             $('.banner-content p a').on('touchstart', function(e){
                 // $('.bx-controls').removeClass('disabled');
                 console.log('touch-start');
                 if (e.type !== 'touchmove' && e.button !== 0) {
                     console.log('touch-move detected');
+                    console.log('X cor is: '+ prevTouchX);
+                console.log('Y cor is: '+ prevTouchY);
                     e.preventDefault();
                 }
                 e.preventDefault();
                 console.log($(this).attr('href'));
                 var x = $(this).attr('href');
                 location.href = x;
+            });
+
+            $('.banner-content p a').on('touchmove', function(e){
+                console.log('touch move detected');
+                e.preventDefault();
             });
 
             
