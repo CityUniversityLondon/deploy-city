e3627a6f35d2175eced4afdc6e7328dc8d7e7505
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-14 11:50:34 +0000

    01/14/19 11:50:04 | JS: touch record changes

--- f-bxslider-issue/js/main/funcs/bxslider-override.js
+++ f-bxslider-issue/js/main/funcs/bxslider-override.js
@@ -3,61 +3,63 @@ var $ = require('../jquery');
 module.exports = function () {
     
     init = function () {
-        $('.banner-content a').length;
+        $(document).ready(function(){
+            console.log($('.banner-content a').length);
+                    
+            $('.banner-content a').on('click', 'touchend', function(e){
+                $('.bx-controls').removeClass('disabled');
+                console.log('clicked');
                 
-        $('.banner-content a').on('click', 'touchend', function(e){
-            $('.bx-controls').removeClass('disabled');
-            console.log('clicked');
+                e.preventDefault();
+                console.log($(this).attr('href'));
+                var x = $(this).attr('href');
+                location.href = x;
+            });
+
             
-            e.preventDefault();
-            console.log($(this).attr('href'));
-            var x = $(this).attr('href');
-            location.href = x;
-        });
 
-        
+            $('.banner-content p a').on('touchend', function(e){
+                $('.bx-controls').removeClass('disabled');
 
-        $('.banner-content p a').on('touchend', function(e){
-            $('.bx-controls').removeClass('disabled');
+                console.log('touch-End');
+                
+                e.preventDefault();
+                console.log($(this).attr('href'));
+                var x = $(this).attr('href');
+                location.href = x;
+            });
 
-            console.log('touch-End');
-            
-            e.preventDefault();
-            console.log($(this).attr('href'));
-            var x = $(this).attr('href');
-            location.href = x;
-        });
+            $('.banner-content p a').on('touchstart', function(e){
+                $('.bx-controls').removeClass('disabled');
+                console.log('touch-start');
+                
+                e.preventDefault();
+                console.log($(this).attr('href'));
+                var x = $(this).attr('href');
+                location.href = x;
+            });
 
-        $('.banner-content p a').on('touchstart', function(e){
-            $('.bx-controls').removeClass('disabled');
-            console.log('touch-start');
-            
-            e.preventDefault();
-            console.log($(this).attr('href'));
-            var x = $(this).attr('href');
-            location.href = x;
-        });
+            $('.bx-wrapper .bx-viewport').on('touchend', function(e){
+                $('.bx-controls').removeClass('disabled');
 
-        $('.bx-wrapper .bx-viewport').on('touchend', function(e){
-            $('.bx-controls').removeClass('disabled');
+                console.log('bx-viewport - touch-End');
+                
+                e.preventDefault();
+                console.log($(this).find('.banner-content p a').attr('href'));
+                var x = $(this).find('.banner-content p a').attr('href');
+                location.href = x;
+            });
 
-            console.log('bx-viewport - touch-End');
-            
-            e.preventDefault();
-            console.log($(this).find('.banner-content p a').attr('href'));
-            var x = $(this).find('.banner-content p a').attr('href');
-            location.href = x;
-        });
+            $('.bx-wrapper .bx-viewport').on('touch-start', function(e){
+                $('.bx-controls').removeClass('disabled');
 
-        $('.bx-wrapper .bx-viewport').on('touch-start', function(e){
-            $('.bx-controls').removeClass('disabled');
-
-            console.log('bx-viewport - touch-start');
-            
-            e.preventDefault();
-            console.log($(this).find('.banner-content p a').attr('href'));
-            var x = $(this).find('.banner-content p a').attr('href');
-            location.href = x;
+                console.log('bx-viewport - touch-start');
+                
+                e.preventDefault();
+                console.log($(this).find('.banner-content p a').attr('href'));
+                var x = $(this).find('.banner-content p a').attr('href');
+                location.href = x;
+            });
         });
 
 
