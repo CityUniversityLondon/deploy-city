0e7bed6a29b5ef185ddb9fde18307e01da8300eb
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-15 13:28:32 +0000

    01/15/19 13:28:10 | JS: touch events updated

--- f-bxslider-issue/js/main/funcs/bxslider-override.js
+++ f-bxslider-issue/js/main/funcs/bxslider-override.js
@@ -4,48 +4,49 @@ module.exports = function () {
     
     init = function () {
         $(document).ready(function(){
-            var prevTouchX;
-            var prevTouchY;
-            console.log('X cor is: '+ prevTouchX);
-            console.log('Y cor is: '+ prevTouchY);
+            var strTouchX;
+            var strTouchY;
+            var endTouchX;
+            var endTouchY;
+            
+            function myTouchSt(e){
+                strTouchX = e.touches[0].clientX;
+                strTouchY = e.touches[0].clientY;
+                console.log('Start X cor is: '+ strTouchX);
+                console.log('STart Y cor is: '+ strTouchY);
+            };
 
-            $(document).on('touchstart', function(e){
-                prevTouchX = e.touches[0].clientX;
-                prevTouchY = e.touches[0].clientY;
-                console.log('X cor is: '+ prevTouchX);
-                console.log('Y cor is: '+ prevTouchY);
+            function myTouchEn(linkLoc){
+                endTouchX = event.changedTouches[0].pageX;
+                endTouchY = event.changedTouches[0].pageY;
+                console.log('End X cor is: '+ endTouchX);
+                console.log('End Y cor is: '+ endTouchY);
+                console.log('----------------------------------');
 
-            });
+                if(endTouchX == strTouchX){
+                    console.log('*** X cors are same! ***');
+                    console.log('Redirecting to : '+ $(this).attr('href'));
+                
+                location.href = linkLoc;
+                };
+            };
+
+            document.ontouchstart = myTouchSt;
 
             console.log($('.banner-content a').length);
-            console.log($('.bx-wrapper .bx-viewport').length);
+            console.log($('.bx-viewport').length);
                     
-            $('.banner-content p a').on('touchend', function(e){
-                // $('.bx-controls').removeClass('disabled');
+            $('.banner-content p a').on('touchend', function(event){
+                $('.bx-controls').removeClass('disabled');
                 console.log('Banner A: touch-end');
-                e.preventDefault();
+                event.preventDefault();
+                var linkLoc = $(this).attr('href');
+                myTouchEn(linkLoc);
 
-                console.log('prev X cor is: '+ prevTouchX);
-                console.log('prev Y cor is: '+ prevTouchY);
-                touchX = e.touches[0].clientX;
-                TouchY = e.touches[0].clientY;
-                console.log('X cor is: '+ prevTouchX);
-                console.log('Y cor is: '+ prevTouchY);
-
-                console.log('Redirecting to : '+ $(this).attr('href'));
-                var x = $(this).attr('href');
-                location.href = x;
             });
-
-            $('.banner-content p a').on('touchmove', function(e){
-                console.log('touch move detected');
-                e.preventDefault();
-            });
-
-            
+          /// doesnt pick up the news elements using this class
             $('.bx-wrapper .bx-viewport').on('touchstart', function(e){
                 $('.bx-controls').removeClass('disabled');
-
                 console.log('bx-viewport - touch-start');
                 
             });
