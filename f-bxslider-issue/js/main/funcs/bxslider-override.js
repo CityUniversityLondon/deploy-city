37be7f84e5abafb3d5267b4e6950b0668a094002
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-16 14:51:21 +0000

    01/16/19 14:50:46 | JS: touch event updates

--- f-bxslider-issue/js/main/funcs/bxslider-override.js
+++ f-bxslider-issue/js/main/funcs/bxslider-override.js
@@ -4,55 +4,74 @@ module.exports = function () {
     
     init = function () {
         $(document).ready(function(){
-            console.log($('.banner-content a').length);
-            console.log(document.getElementsByClassName('bx-wrapper').length);
-            console.log($('.home-news-events .bx-wrapper').length);
-            
-            var strTouchX;
-            var strTouchY;
-            var endTouchX;
-            var endTouchY;
-            
-            function myTouchStr(e){
-                strTouchX = e.touches[0].clientX;
-                strTouchY = e.touches[0].clientY;
-                console.log('start touch is: '+strTouchX);      
-            };
 
-            document.ontouchstart = myTouchStr;
-            
-            var i;
-            for (i=0; i < $('.banner-content a').length; i++){
-
-                document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(event){
-                    $('.bx-controls').removeClass('disabled');
-                    console.log('*** Banner A: touch-end');
-                    console.log(this.href);
-                    event.preventDefault();
-
-                    endTouchX = event.changedTouches[0].pageX;
-                    console.log(endTouchX);
-
-                    if ( endTouchX == strTouchX){
-                        console.log('**touch values same');
-                        location.href = this.href;
-                        console.log('----------------------------');
-                    }
-                    else{
-                        console.log('touch values different');
-                        console.log('----------------------------');
-                    };
-                });
-            };         
-          /// doesnt pick up the news elements using this class
-          $('.bx-viewport').on('touchstart', function(e){
-                $('.bx-controls').removeClass('disabled');
+            if(location.href == 'https://www.city.ac.uk/'){
+                console.log($('.banner-content a').length);
+                console.log(document.getElementsByClassName('bx-wrapper').length);
                 
-                console.log('*** bx-viewport - touch-start ***');
-            });  
+                /**** News slider (only on mobile) ****/
+                // doesnt pick up the news elements using this class
+                setTimeout(function(){ 
+                    $('.bx-viewport').on('touchstart', function(e){
+                        // re-instates controls after being deactivated by bxslider node module    
+                        $('.bx-controls').removeClass('disabled');
+                            
+                            console.log('*** bx-viewport - touch-start ***');
+                    });
 
-        });
-    };
+                    $('.bx-controls-direction a').on('touchend', function(e){
+                        // re-instates controls after being deactivated by bxslider node module    
+                        $('.bx-controls').removeClass('disabled');
+                            
+                            console.log('*** box controls touched ***');
+                    });
+
+
+                }, 1000);
+                
+                var strTouchX;
+                var strTouchY;
+                var endTouchX;
+                var endTouchY;
+                
+                function myTouchStr(e){
+                    strTouchX = e.touches[0].clientX;
+                    strTouchY = e.touches[0].clientY;
+                    console.log('start touch is: '+strTouchX);   
+                    console.log(e.target.tagName);   
+                };
+
+                document.ontouchstart = myTouchStr;
+                
+                /**** Home page top slider ****/
+                var i;
+                for (i=0; i < $('.banner-content a').length; i++){
+
+                    document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(event){
+                        $('.bx-controls').removeClass('disabled');
+                        console.log('*** Banner A: touch-end');
+                        event.preventDefault();
+
+                        endTouchX = event.changedTouches[0].pageX;
+                        console.log(endTouchX);
+
+                        if ( endTouchX == strTouchX){
+                            console.log('**touch values same');
+                            location.href = this.href;
+                            console.log('----------------------------');
+                        }
+                        else{
+                            console.log('touch values different');
+                            console.log('----------------------------');
+                        };
+                    });
+                };// end for loop   
+
+                
+            };      
+
+        }); // end page ready func
+    }; // end init func
 
     return init();
 
