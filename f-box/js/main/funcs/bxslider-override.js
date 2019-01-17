8b6a5a330a4f8fdd54955d592ea9c9bf24bbe8e7
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-17 15:30:28 +0000

    01/17/19 15:29:56 | JS: restored to previous working version

--- f-box/js/main/funcs/bxslider-override.js
+++ f-box/js/main/funcs/bxslider-override.js
@@ -4,37 +4,55 @@ module.exports = function () {
     
     init = function () {
         $(document).ready(function(){
-            console.log($('.banner-content a').length);
-            if(location.href == 'https://www.city.ac.uk/'){            
+
+            if(location.href == 'https://www.city.ac.uk/'){
+                console.log($('.banner-content a').length);
+                console.log(document.getElementsByClassName('bx-wrapper').length);
+                
+                /**** News slider (only on mobile) ****/
+                // doesnt pick up the news elements using this class
+                setTimeout(function(){ 
+                    $('.bx-viewport').on('touchstart', function(e){
+                        // re-instates controls after being deactivated by bxslider node module    
+                        $('.bx-controls').removeClass('disabled');
+                            
+                            console.log('*** bx-viewport - touch-start ***');
+                    });
+
+                    $('.bx-controls-direction a').on('touchend', function(e){
+                        // re-instates controls after being deactivated by bxslider node module    
+                        $('.bx-controls').removeClass('disabled');
+                            
+                            console.log('*** box controls touched ***');
+                    });
+
+
+                }, 1000);
+                
                 var strTouchX;
                 var strTouchY;
                 var endTouchX;
                 var endTouchY;
                 
-                document.addEventListener("touchstart", function(e){
-
+                function touchStartCoordinates(e){
                     strTouchX = e.touches[0].clientX;
                     strTouchY = e.touches[0].clientY;
-                    console.log('TouchStart X cor is: '+strTouchX);   
-                    console.log(e.target.tagName +' element was touched');
-                    // re-instates controls after being deactivated by bxslider node module 
-                    $('.bx-controls, .bx-has-controls-direction').removeClass('disabled');
+                    console.log('start touch is: '+strTouchX);   
+                    console.log(e.target.tagName);   
+                };
 
-                }); 
-
-                document.ontouchstart = myTouchStr;
-
-        
+                document.ontouchstart = touchStartCoordinates;
+                
                 /**** Home page top slider ****/
                 var i;
                 for (i=0; i < $('.banner-content a').length; i++){
 
-                    document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(event){
-                        event.preventDefault();
+                    document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(e){
                         $('.bx-controls').removeClass('disabled');
                         console.log('*** Banner A: touch-end');
-                        
-                        endTouchX = event.changedTouches[0].pageX;
+                        e.preventDefault();
+
+                        endTouchX = e.changedTouches[0].pageX;
                         console.log(endTouchX);
 
                         if ( endTouchX == strTouchX){
