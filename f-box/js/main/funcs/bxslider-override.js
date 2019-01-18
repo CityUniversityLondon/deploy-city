9518e53378201c3ee4b1605439528e77f1504220
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-18 14:15:41 +0000

    01/18/19 14:15:03 | JS: added touch events override for news slider

--- f-box/js/main/funcs/bxslider-override.js
+++ f-box/js/main/funcs/bxslider-override.js
@@ -5,24 +5,29 @@ module.exports = function () {
     init = function () {
         $(document).ready(function(){
             console.log($('.banner-content a').length);
+            console.log($('.news-card-content__title').length);
             if(location.href == 'https://www.city.ac.uk/?dev=box'){            
                 var strTouchX;
                 var endTouchX;
-                                
+                
+                // records touch coordinates for determining swipe or touch
                 document.addEventListener("touchstart", function(e){
                     strTouchX = e.touches[0].clientX;
                     strTouchY = e.touches[0].clientY; //to be removed
                     console.log('TouchStart X cor is: '+strTouchX);   // to be removed
-                    console.log(e.target.tagName +' element was touched'); // to be removed
+                    console.log(e.target.className);
+                    
                     // re-instates controls after being deactivated by bxslider node module 
                     $('.bx-controls, .bx-has-controls-direction').removeClass('disabled');
                 }); 
 
-                //document.ontouchstart = myTouchStr;
-
-        
+                isTouchClick = endTouchX => endTouchX == strTouchX ? true : false;
+                
                 /**** Home page top slider ****/
-                /*var i;
+
+                // try add another event listener for the news links...
+
+                var i;
                 for (i=0; i < $('.banner-content a').length; i++){
                     document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(e){
                         e.preventDefault();
@@ -31,17 +36,30 @@ module.exports = function () {
                         
                         endTouchX = e.changedTouches[0].pageX;
                         console.log(endTouchX);
-
-                        if ( endTouchX == strTouchX){
-                            console.log('**touch values same');
+                        if (isTouchClick(endTouchX)){ // calls is click function to determine if click or swipe to place
                             location.href = this.href;
-                            console.log('----------------------------');
-                        } else{
-                            console.log('touch values different');
-                            console.log('----------------------------');
+                            console.log('reee-directing');
                         };
+                       
                     });
-                };// end for loop   */
+                };// end for loop  
+
+                for (i=0; i< $('.news-card-content__title').length; i++){
+                    document.getElementsByClassName('news-card-content__title')[i].addEventListener("touchend", function(e){
+                        // e.preventDefault();
+                        $('.bx-controls').removeClass('disabled');
+                        console.log('*** Banner A: touch-end');
+                        
+                        endTouchX = e.changedTouches[0].pageX;
+                        console.log(endTouchX);
+                        console.log(this.parentNode.href);
+                        if (isTouchClick(endTouchX)){ // calls is click function to determine if click or swipe to place
+                            location.href = this.parentNode.href;
+                            console.log('reee-directing');
+                        };
+                       
+                    });
+                };// end for loop 
 
                 
             };      
