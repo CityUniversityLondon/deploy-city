63f4e21e37304e985bf01258598226829776c992
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-18 15:59:37 +0000

    01/18/19 15:59:13 | Release V1.1

--- f-box/js/main/funcs/bxslider-override.js
+++ f-box/js/main/funcs/bxslider-override.js
@@ -1,68 +1,62 @@
+//
+// This is to override the touch issues experienced with the node mudule: Box slider. The box slider's carousels 
+// found on the homepage have issue with the text links found on each of the slides. When clicked / touching these links 
+// on mobile phones or tablets nothing happens. I suspect the carousel was designed to only contain picture slides
+// without text. The module be detault disables some touch events. This code re-instate touch events on the links. 
+// This code targets two carousels, namely the one at the very top of the home page and a news one at the bottom.
+// The latter only loads as a carousel on small view ports 
+//
 var $ = require('../jquery');
 
 module.exports = function () {
     
     init = function () {
         $(document).ready(function(){
-            console.log($('.banner-content a').length);
-            console.log($('.news-card-content__title').length);
-            if(location.href == 'https://www.city.ac.uk/?dev=box'){            
+                        
+            if(location.href == 'https://www.city.ac.uk/'){            
                 var strTouchX;
                 var endTouchX;
                 
                 // records touch coordinates for determining swipe or touch
                 document.addEventListener("touchstart", function(e){
                     strTouchX = e.touches[0].clientX;
-                    strTouchY = e.touches[0].clientY; //to be removed
-                    console.log('TouchStart X cor is: '+strTouchX);   // to be removed
-                    console.log(e.target.className);
-                    
+                                        
                     // re-instates controls after being deactivated by bxslider node module 
                     $('.bx-controls, .bx-has-controls-direction').removeClass('disabled');
                 }); 
 
+                // determines if touch is click or swipe by comparing start touch and end values
                 isTouchClick = endTouchX => endTouchX == strTouchX ? true : false;
                 
                 /**** Home page top slider ****/
-
-                // try add another event listener for the news links...
 
                 var i;
                 for (i=0; i < $('.banner-content a').length; i++){
                     document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(e){
                         e.preventDefault();
-                        $('.bx-controls').removeClass('disabled');
-                        console.log('*** Banner A: touch-end');
-                        
+                        $('.bx-controls').removeClass('disabled');                       
                         endTouchX = e.changedTouches[0].pageX;
-                        console.log(endTouchX);
+                        
                         if (isTouchClick(endTouchX)){ // calls is click function to determine if click or swipe to place
                             location.href = this.href;
-                            console.log('reee-directing');
-                        };
-                       
+                        };  
                     });
                 };// end for loop  
 
+                /**** Home page news slider (only on mobiles) ****/
+
                 for (i=0; i< $('.news-card-content__title').length; i++){
                     document.getElementsByClassName('news-card-content__title')[i].addEventListener("touchend", function(e){
-                        // e.preventDefault();
                         $('.bx-controls').removeClass('disabled');
-                        console.log('*** Banner A: touch-end');
-                        
                         endTouchX = e.changedTouches[0].pageX;
-                        console.log(endTouchX);
-                        console.log(this.parentNode.href);
+
                         if (isTouchClick(endTouchX)){ // calls is click function to determine if click or swipe to place
                             location.href = this.parentNode.href;
-                            console.log('reee-directing');
                         };
-                       
                     });
-                };// end for loop 
+                };// end for loop   
 
-                
-            };      
+            }; // end if       
 
         }); // end page ready func
     }; // end init func
