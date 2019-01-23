9301dc5ec05ec0cbf87df9475cac5b2d863c4bc1
CUL Web Team <ucs-webteam@city.ac.uk>
2019-01-23 11:09:42 +0000

    01/23/19 11:09:18 | JS: tidied up js

--- f-box-fix/js/mods/boxslider-override.js
+++ f-box-fix/js/mods/boxslider-override.js
@@ -1,61 +1,65 @@
 //
-// This is to override the touch issues experienced with the node mudule: Box slider. The box slider's carousels 
-// found on the homepage have issue with the text links found on each of the slides. When clicked / touching these links 
+// This is to override the touch issues experienced with the node mudule: Box slider. The box slider's carousels
+// found on the homepage have issue with the text links found on each of the slides. When clicked / touching these links
 // on mobile phones or tablets nothing happens. I suspect the carousel was designed to only contain picture slides
-// without text. The module be detault disables some touch events. This code re-instate touch events on the links. 
+// without text. The module be detault disables some touch events. This code re-instate touch events on the links.
 // This code targets two carousels, namely the one at the very top of the home page and a news one at the bottom.
-// The latter only loads as a carousel on small view ports 
+// The latter only loads as a carousel on small view ports
 //
-/**/
+
 var $ = require('./libs/jquery'),
     defer = require('./utils/defer'),
-    
-    init = function () {
-        $(document).ready(function(){
-                                   
-            var strTouchX;
-            var endTouchX;
-            
-            // records touch coordinates for determining swipe or touch
-            document.addEventListener("touchstart", function(e){
-                strTouchX = e.touches[0].clientX;
-                                    
-                // re-instates controls after being deactivated by bxslider node module 
-                $('.bx-controls, .bx-has-controls-direction').removeClass('disabled');
-            }); 
+    init = function() {
+        var strTouchX;
+        var endTouchX;
 
-            // determines if touch is click or swipe by comparing start touch and end values
-            isTouchClick = endTouchX => endTouchX == strTouchX ? true : false;
-            
-            /**** Home page top slider ****/
+        // records touch coordinates for determining swipe or touch
+        document.addEventListener('touchstart', function(e) {
+            strTouchX = e.touches[0].clientX;
 
-            var i;
-            for (i=0; i < $('.banner-content a').length; i++){
-                document.getElementsByClassName('banner-content')[i].getElementsByTagName('a')[0].addEventListener("touchend", function(e){
+            // re-instates controls after being deactivated by bxslider node module
+            $('.bx-controls, .bx-has-controls-direction').removeClass(
+                'disabled'
+            );
+        });
+
+        // determines if touch is click or swipe by comparing start touch and end values
+        isTouchClick = endTouchX => (endTouchX == strTouchX ? true : false);
+
+        /**** Home page top slider ****/
+
+        var i;
+        for (i = 0; i < $('.banner-content a').length; i++) {
+            document
+                .getElementsByClassName('banner-content')
+                [i].getElementsByTagName('a')[0]
+                .addEventListener('touchend', function(e) {
                     e.preventDefault();
-                    $('.bx-controls').removeClass('disabled');                       
-                    endTouchX = e.changedTouches[0].pageX;
-                    
-                    if (isTouchClick(endTouchX)){ // calls isTouchClick function to determine if click or swipe to place
-                        location.href = this.href;
-                    }  
-                });
-            }// end for loop  
-
-            /**** Home page news slider (only on mobiles) ****/
-
-            for (i=0; i< $('.news-card-content__title').length; i++){
-                document.getElementsByClassName('news-card-content__title')[i].addEventListener("touchend", function(e){
                     $('.bx-controls').removeClass('disabled');
                     endTouchX = e.changedTouches[0].pageX;
 
-                    if (isTouchClick(endTouchX)){ // calls isTouchClick function to determine if click or swipe to place
+                    if (isTouchClick(endTouchX)) {
+                        // calls isTouchClick function to determine if click or swipe to place
+                        location.href = this.href;
+                    }
+                });
+        } // end for loop
+
+        /**** Home page news slider (only on mobiles) ****/
+
+        for (i = 0; i < $('.news-card-content__title').length; i++) {
+            document
+                .getElementsByClassName('news-card-content__title')
+                [i].addEventListener('touchend', function(e) {
+                    $('.bx-controls').removeClass('disabled');
+                    endTouchX = e.changedTouches[0].pageX;
+
+                    if (isTouchClick(endTouchX)) {
+                        // calls isTouchClick function to determine if click or swipe to place
                         location.href = this.parentNode.href;
                     }
                 });
-            }// end for loop         
+        } // end for loop
+    }; // end init func
 
-        }); // end page ready func 
-     }; // end init func
-
-defer(init); /**/
+defer(init);
