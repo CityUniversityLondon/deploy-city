3c7256e45cffc8251971d6432fbc9067216c7b5e
CUL Web Team <ucs-webteam@city.ac.uk>
2021-08-05 15:04:13 +0000

    08/05/21 15:03:42 | test

--- f-wr-research-centre/js/main/funcs/correct.js
+++ f-wr-research-centre/js/main/funcs/correct.js
@@ -3,7 +3,7 @@ module.exports = function () {
 
     return function() {
         console.log('test');
-        //document.getElementsByTagName('h1')[0].style.backgroundColor = "red";
+        document.getElementsByTagName('h1')[0].style.backgroundColor = "red";
     };
 
 }();
