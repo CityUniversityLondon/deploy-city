24639a8996254128012af97125038962fdb6c36f
CUL Web Team <ucs-webteam@city.ac.uk>
2022-01-06 11:04:04 +0000

    01/06/22 11:03:02 | renamed threads to threats

--- f-wr-url-snitise/js/utils/url-sanitise.js
+++ f-wr-url-snitise/js/utils/url-sanitise.js
@@ -4,7 +4,7 @@ This function is inserted in the head section of the website. It's to check if a
 function urlSanitise() {
     if (typeof window !== 'undefined') {
         let urlLocation = window.location.toString();
-        let threads = 0;
+        let threats = 0;
         const maliciousTerms = ['%3Cscript', '%3C/script'];
 
         maliciousTerms.forEach(function(term) {
@@ -12,11 +12,11 @@ function urlSanitise() {
 
             if (found > 0) {
                 urlLocation = urlLocation.slice(0, found);
-                threads += 1;
+                threats += 1;
             }
         });
 
-        threads > 0 ? (window.location.href = urlLocation) : null;
+        threats > 0 ? (window.location.href = urlLocation) : null;
     }
 }
 
