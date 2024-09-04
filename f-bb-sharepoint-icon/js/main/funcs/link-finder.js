183442abb053a5b425717393af7c99bff07da76b
CUL Web Team <ucs-webteam@city.ac.uk>
2024-09-04 12:49:44 +0000

    09/04/24 12:49:12 | comments added

--- f-bb-sharepoint-icon/js/main/funcs/link-finder.js
+++ f-bb-sharepoint-icon/js/main/funcs/link-finder.js
@@ -1,11 +1,21 @@
 module.exports = function () {
+
+
 /**
- * Loop over every link and apply nessessary classes
+ * Adds a FontAwesome icon (or any other classes) as a <span> element to the specified DOM element.
+ * The icon can be appended to the end or prepended to the beginning of the element based on the append flag.
  *
+ * @param {HTMLElement} element - The DOM element to which the icon will be added. It must be a valid HTMLElement.
+ * @param {string|string[]} classes - A string or an array of strings representing the class(es) to be added to the icon's <span> element. If a single class is passed, it will be added directly. If an array is passed, all classes will be added.
+ * @param {boolean} [append=false] - A boolean flag indicating whether the icon should be appended or prepended.
+ *                                   If true, the icon will be appended to the end of the element. 
+ *                                   If false or not provided, the icon will be prepended to the beginning.
+ *
+ * Usage:
+ * addIcon(element, 'fa-kit', true);       // Appends a single class icon
+ * addIcon(element, ['fa-kit', 'fa-share'], false); // Prepends multiple classes
  */
-
-
-    function addIcon(element, classes, pos = 'prepend') {
+    function addIcon(element, classes, append = false) {
         console.log('ddd')
         if (element && classes) {
             // Create span for FA icon
@@ -17,32 +27,38 @@ module.exports = function () {
             } else if (Array.isArray(classes)) {
                 classes.forEach(cls => spanNode.classList.add(cls));  // Add multiple classes
             }
-
+   
             // Append or prepend the icon based on the `pos` parameter
-            if (pos === 'append') {
-                element.append(spanNode); // Adds icon to the beginning
+            if (append) {
+                element.append(' ',spanNode); // Adds icon to the beginning
             } else {
-                element.prepent(spanNode);  // Adds icon to the end
+                element.prepend(spanNode);  // Adds icon to the end
             }
         }
     }
 
+/**
+ * Checks if provided element is a sharepoint link and if so adds the fa-sharepoint icon.
+ *
+*/
     function findSharepointLink(anchor){
         const url = new URL(anchor.href);
         if (url.hostname.endsWith('sharepoint.com')) {
-
-            
-
             const firstChild = anchor.firstChild;
-            console.log(firstChild)
             if (firstChild && firstChild.nodeType === 3) {
-                //Text is the first node of the anchor, so no icons are already being used
-                addIcon(anchor,[fa-kit,fa-sharepoint])
-              }
-            
+                //Only add the icon if first child of anchor is a text node, this will avoid adding an icon when one is already in place.
+                addIcon(anchor,['fa-kit','fa-sharepoint']);
+
+                addIcon(anchor,['fa','fa-external-link'], true);
+              }   
         }
     }
 
+
+    /**
+     * Loop over every link and apply nessessary classes
+     *
+     */
     function findLinks() {
         const anchors = document.querySelectorAll('a');
 
